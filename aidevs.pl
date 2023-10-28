f6d44c4da4c9f1c0080a6c2fa63fafed178a5fb4
Piotr <73885173+lolgrend@users.noreply.github.com>
2023-10-29 00:04:31 +0200

    Create aidevs.pl

---
+++ aidevs.pl
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Moja ulubiona pizza</title>
+</head>
+<body>
+  <h1>Witaj na mojej stronie o pizzy!</h1>
+aidevs.pl
+aidevs.pl
+aidevs
+  <p>Pizza jest cudownym daniem pochodzącym z Włoch, które zyskało popularność na całym świecie. Składa się głównie z ciasta, sosu pomidorowego, sera i różnych dodatków takich jak warzywa, owoce morza czy mięso. Moją ulubioną pizzą jest Margherita, ponieważ uwielbiam prostotę i delikatność smaku świeżych składników - pomidorów, mozzarelli i bazylii. Nie można zapomnieć o cienkim, chrupiącym cieście, które jest kluczem do stworzenia idealnej pizzy. Spróbuj raz i zakochasz się na zawsze!</p>
+aidevs.pl
+</body>
+</html>
